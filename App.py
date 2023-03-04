d637ca5aa2c3c24ee3c3a1cd33359b7c2a8fad9a
Disgusk <ishaq@uni.minerva.edu>
2023-03-04 13:22:40 -0800

    Working Pronunciation Check

--- App.py
+++ App.py
@@ -2,41 +2,31 @@ import os
 import azure.cognitiveservices.speech as speechsdk
 import json
 
-def recognize_from_microphone():
-    # This example requires environment variables named "SPEECH_KEY" and "SPEECH_REGION"
+def pron_check_and_read_from_file():
     speech_config = speechsdk.SpeechConfig(subscription=os.environ.get('SPEECH_KEY'), region=os.environ.get('SPEECH_REGION'))
-    speech_config.speech_recognition_language="en-US"
-
-    audio_config = speechsdk.audio.AudioConfig(use_default_microphone=True)
+    audio_config = speechsdk.AudioConfig(filename="sample.wav")
     speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, audio_config=audio_config)
-
-    print("Speak into your microphone.")
-    speech_recognition_result = speech_recognizer.recognize_once_async().get()
-
-    if speech_recognition_result.reason == speechsdk.ResultReason.RecognizedSpeech:
-        print("Recognized: {}".format(speech_recognition_result.text))
-    elif speech_recognition_result.reason == speechsdk.ResultReason.NoMatch:
-        print("No speech could be recognized: {}".format(speech_recognition_result.no_match_details))
-    elif speech_recognition_result.reason == speechsdk.ResultReason.Canceled:
-        cancellation_details = speech_recognition_result.cancellation_details
+    result = speech_recognizer.recognize_once_async().get()
+    if result.reason == speechsdk.ResultReason.RecognizedSpeech:
+        print("Recognized: {}".format(result.text))
+    elif result.reason == speechsdk.ResultReason.NoMatch:
+        print("No speech could be recognized: {}".format(result.no_match_details))
+    elif result.reason == speechsdk.ResultReason.Canceled:
+        cancellation_details = result.cancellation_details
         print("Speech Recognition canceled: {}".format(cancellation_details.reason))
         if cancellation_details.reason == speechsdk.CancellationReason.Error:
             print("Error details: {}".format(cancellation_details.error_details))
             print("Did you set the speech resource key and region values?")
-    return [speech_recognition_result, speech_recognition_result.text]
+    ref_text = result.text
+    speech_recognizer2 =  speechsdk.SpeechRecognizer(speech_config=speech_config, audio_config=audio_config)
+    pronunciation_assessment_config = speechsdk.PronunciationAssessmentConfig(json_string="{\"referenceText\":\"%s\",\"gradingSystem\":\"HundredMark\",\"granularity\":\"Phoneme\"}" %ref_text)
+    pronunciation_assessment_config.apply_to(speech_recognizer2)
+    result2 = speech_recognizer2.recognize_once()
+    pronunciation_assessment_result_json = result2.properties.get(speechsdk.PropertyId.SpeechServiceResponse_JsonResult)
+    json_results_pron = json.loads(pronunciation_assessment_result_json)
+    print(json.dumps(json_results_pron, indent =4))
 
-def pron_check(reference_text):
-    pronunciation_assessment_config = speechsdk.PronunciationAssessmentConfig(json_string="{\"ReferenceText\":\"%r\",\"gradingSystem\":\"HundredMark\",\"granularity\":\"Phoneme\"}" %reference_text[1])
-    
-    pronunciation_assessment_config.apply_to(reference_text[0])
-    print(reference_text[0])
-    #speech_recognition_result = reference_text[0].recognize_once()
-
-    # The pronunciation assessment result as a JSON string
-    pronunciation_assessment_result_json = speech_recognition_result.properties.get(speechsdk.PropertyId.SpeechServiceResponse_JsonResult)
-    print(pronunciation_assessment_result_json)
-
-def pron_check2():
+def pron_check_from_microphone():
         pronunciation_assessment_config = speechsdk.PronunciationAssessmentConfig(json_string="{\"ReferenceText\":\"Hello my name is George\",\"gradingSystem\":\"HundredMark\",\"granularity\":\"Phoneme\"}")
         speech_config = speechsdk.SpeechConfig(subscription=os.environ.get('SPEECH_KEY'), region=os.environ.get('SPEECH_REGION'))
         speech_config.speech_recognition_language="en-US"
@@ -51,5 +41,4 @@ def pron_check2():
         pronunciation_assessment_result_json = speech_recognition_result.properties.get(speechsdk.PropertyId.SpeechServiceResponse_JsonResult)
         print(pronunciation_assessment_result_json)
 
-pron_check(recognize_from_microphone())
-#pron_check2()
+pron_check_and_read_from_file()
